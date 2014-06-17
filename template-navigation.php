a9a2925a65b5097f929ef574d30ed2f6e4d6e3af
Rachel Reveley <rachel.reveley@lovehoney.co.uk>
2014-06-17 09:42:48 +0100

    Refactor flyout code so flyouts can be used in other places

---
+++ template-navigation.php
@@ -0,0 +1,111 @@
+<?php include("header.php"); ?>
+		<style type="text/css">
+			.parent {background-color: #fcfcfc;}
+			.parent > div {background-color: #eee;}
+		</style>
+					<div class="parent dt-1-1 tb-1-1 mb-1-1 collapse-top">
+						<div>
+							<nav>
+								<ul>
+									<li><a href="index.php">Home</a></li>
+									<li><a href="template-widgets.php">Standard Widets</a></li>
+									<li><a href="template-grid-sizes.php">Grid system</a></li>
+									<li><a href="template-forms.php">Forms</a></li>
+									<li><a href="template-type.php">Type</a></li>
+									<li><a href="template-non-semantic.php">Other widgets</a></li>
+									<li><a href="themes.php">Themes</a></li>
+									<li><a href="https://github.com/rachelreveley/css-cement">Github</a></li>
+								</ul>
+							</nav>
+						</div>
+						<div>
+							<div class="parent dt-1-1 tb-1-1 mb-1-1 collapse-top">
+								<div>
+									<h1>The grid system</h1>
+									<p>The grid system is based on fractions and allows from 1 to 20 columns of regular or irregular sizes.</p>
+									<p>The grid's margins and padding are configurable.</p>
+									<p>The grid has 3 break point sizes, desktop, tablet and mobile.</p>
+									<p>Grid elements can override the size set on their parent container.</p>
+								</div>
+							</div>
+							<div class="parent dt-1-1 tb-1-20 mb-1-1 collapse-top">
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-2 tb-1-16 mb-1-2 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-3 tb-1-12 mb-1-3 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-4 tb-1-10 mb-1-1 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-5 tb-1-8 mb-1-3 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-6 tb-1-6 mb-1-1 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-8 tb-1-5 mb-1-3 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-10 tb-1-4 mb-1-1 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-12 tb-1-3 mb-1-3 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-16 tb-1-2 mb-1-1 collapse-top">
+								<div>&nbsp;</div>
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-20 tb-1-1 mb-1-3 collapse-top">
+								<div>&nbsp;</div>
+							</div>
+							<div class="parent dt-1-2 tb-1-1 mb-1-1 collapse-top">
+								<div class="dt-1-4 tb-1-4 mb-1-4">&nbsp;</div>
+								<div class="dt-1-4 tb-1-4 mb-1-4">&nbsp;</div>
+								<div class="dt-1-2 tb-1-2 mb-1-2 ">&nbsp;</div>
+							</div>
+							<div class="parent dt-1-9 tb-1-1 mb-1-1 collapse-top">
+								<div class="dt-1-3 tb-1-3 mb-1-3">&nbsp;</div>
+								<div class="dt-2-3 tb-2-3 mb-2-3">&nbsp;</div>
+							</div>
+							<div class="parent">
+								<div class="dt-1-4 tb-1-4 mb-1-4">&nbsp;</div>
+								<div class="dt-1-2 tb-1-2 mb-1-2">&nbsp;</div>
+								<div class="dt-1-4 tb-1-4 mb-1-4">&nbsp;</div>
+							</div>
+							<div class="parent">
+								<div class="dt-1-3 tb-1-3 mb-1-3">&nbsp;</div>
+								<div class="dt-1-3 tb-1-3 mb-1-3">&nbsp;</div>
+								<div class="dt-1-6 tb-1-6 mb-1-6">&nbsp;</div>
+								<div class="dt-1-6 tb-1-6 mb-1-6">&nbsp;</div>
+							</div>
+							<div class="parent">
+								<div class="dt-1-2 tb-1-2 mb-1-2">&nbsp;</div>
+								<div class="dt-1-3 tb-1-3 mb-1-3">&nbsp;</div>
+								<div class="dt-1-6 tb-1-6 mb-1-6">&nbsp;</div>
+							</div>
+							<div class="parent">
+								<div class="dt-1-10 tb-1-10 mb-1-10">&nbsp;</div>
+								<div class="dt-9-10 tb-9-10 mb-9-10">&nbsp;</div>
+							</div>
+						</div>
+					</div>
+<?php include("footer.php"); ?>
