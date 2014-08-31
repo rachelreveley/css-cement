1b433fb6f8f0e4e59748c2c15734898a67349818
Rachel Reveley <Rachel.Reveley@lovehoney.co.uk>
2014-08-31 15:14:04 +0100

    Themes page

---
+++ themes.php
@@ -0,0 +1,18 @@
+<?php
+$meta_title="CSS Cement - Themes";
+$slug="Themes";
+?>
+<?php include("header.php"); ?>
+					<div class="parent dt-1-1 tb-1-1 mb-1-1">
+						<h1>Themes</h1>
+						<p>Some of the themes here are basic starter themes designed to make building your own design using CSS Cement easier. We also plan to add some premium themes that you can use as is.</p>
+					</div>
+					<div class="parent dt-1-2 tb-1-2 mb-1-1">
+						<h2>Basic Themes</h2>
+						<div>
+							<h3><a href="">Teal</a></h3>
+							<p>This is a mono coloured theme that could be used as is for a function site such as an admin dashboard or intranet.</p>
+						</div>
+						<div><img src="" alt="Basic Teal CSS Cement Theme" /></div>
+					</div>
+<?php include("footer.php"); ?>
