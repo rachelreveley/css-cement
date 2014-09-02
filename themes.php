1186762eafdbb9f87eac7821faeab6a4b33645a3
Rachel Reveley <Rachel.Reveley@lovehoney.co.uk>
2014-09-02 21:51:37 +0100

    adjust theme page layout

--- themes.php
+++ themes.php
@@ -4,11 +4,13 @@ $slug="Themes";
 ?>
 <?php include("header.php"); ?>
 					<div class="parent dt-1-1 tb-1-1 mb-1-1">
-						<h1>Themes</h1>
-						<p>Some of the themes here are basic starter themes designed to make building your own design using CSS Cement easier. I also plan to add some premium themes that you can use as is.</p>
-					</div>
+						<div>
+							<h1>Themes</h1>
+							<p>Some of the themes here are basic starter themes designed to make building your own design using CSS Cement easier. I also plan to add some premium themes that you can use as is.</p>
+							<h2>Free Themes</h2>
+						</div>
+						</div>
 					<div class="parent dt-1-2 tb-1-2 mb-1-1">
-						<h2>Free Themes</h2>
 						<div>
 							<h3>Teal</h3>
 							<p>This is a mono coloured theme that could be used as is for a function site such as an admin dashboard or intranet.</p>
@@ -19,6 +21,5 @@ $slug="Themes";
 							<p>Berry is a bright cheerful theme that would suit a variety of purposes.</p>
 							<p><a href="/css-cement/template-widgets.php?theme=berry" class="button">Demo</a> <a href="https://github.com/rachelreveley/css-cement-theme-berry" class="button">Download</a></p>
 						</div>
-						<div><img src="" alt="Basic Teal CSS Cement Theme" /></div>
 					</div>
 <?php include("footer.php"); ?>
