94c5f57e2e1ce26867fa8e6c7e2756c8116e3c23
Rachel Reveley <Rachel.Reveley@lovehoney.co.uk>
2014-09-02 20:56:26 +0100

    Update themes page

--- themes.php
+++ themes.php
@@ -5,13 +5,19 @@ $slug="Themes";
 <?php include("header.php"); ?>
 					<div class="parent dt-1-1 tb-1-1 mb-1-1">
 						<h1>Themes</h1>
-						<p>Some of the themes here are basic starter themes designed to make building your own design using CSS Cement easier. We also plan to add some premium themes that you can use as is.</p>
+						<p>Some of the themes here are basic starter themes designed to make building your own design using CSS Cement easier. I also plan to add some premium themes that you can use as is.</p>
 					</div>
 					<div class="parent dt-1-2 tb-1-2 mb-1-1">
-						<h2>Basic Themes</h2>
+						<h2>Free Themes</h2>
 						<div>
-							<h3><a href="">Teal</a></h3>
+							<h3>Teal</h3>
 							<p>This is a mono coloured theme that could be used as is for a function site such as an admin dashboard or intranet.</p>
+							<p><a href="/css-cement/template-widgets.php?theme=teal" class="button">Demo</a> <a href="https://github.com/rachelreveley/css-cement-theme-teal" class="button">Download</a></p>
+						</div>
+						<div>
+							<h3>Berry</h3>
+							<p>Berry is a bright cheerful theme that would suit a variety of purposes.</p>
+							<p><a href="/css-cement/template-widgets.php?theme=berry" class="button">Demo</a> <a href="https://github.com/rachelreveley/css-cement-theme-berry" class="button">Download</a></p>
 						</div>
 						<div><img src="" alt="Basic Teal CSS Cement Theme" /></div>
 					</div>
