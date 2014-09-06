2071984dde501aaaace4864bcc4fdc9a5825c3f5
Rachel Reveley <Rachel.Reveley@lovehoney.co.uk>
2014-09-06 19:46:47 +0100

    News layout template

---
+++ layout-news.php
@@ -0,0 +1,133 @@
+<?php
+$meta_title="CSS Cement - The Grid";
+$slug="The optional grid system";
+?>
+<?php include("header.php"); ?>
+<style type="text/css">
+	.outer-panel {background-color: #f1f1f1;}
+	.panel {background-color: #ccc;}
+	.inner-panel {background-color: #aaa;}
+/* News layout styles */
+	h2 {font-size: 24px; font-weight: bold; line-height: 1.2; margin-bottom: 0;}
+	h1 {font-size: 32px; font-weight: bold; line-height: 1.2; margin-bottom: 0;}
+</style>
+				<div class="parent dt-2-3 tb-2-3 mb-1-1 nested">
+					<div>
+						<div class="parent dt-1-2 tb-1-2 mb-1-2 nested">
+							<div class="dt-1-1 tb-1-1 mb-1-1 no-padding">
+								<h1><a href="">Headline</a></h1>
+								<img alt="Alt text here" src="http://fpoimg.com/200x150?text=Photo" class="left" />
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="">Read more</a></p>
+							</div>
+							<div class="panel">
+								<h2>Heading</h2>
+								<ul>
+									<li><a href="">Lorem ipsum dolor sit amet</a></li>
+									<li><a href="">consectetur adipiscing elit</a></li>
+									<li><a href="">sed do eiusmod tempor incididunt</a></li>
+									<li><a href="">sed do eiusmod tempor incididunt</a></li>
+								</ul>
+							</div>
+							<div class="panel">
+								<h2>Headng</h2>
+								<ul>
+									<li><a href="">Lorem ipsum dolor sit amet</a></li>
+									<li><a href="">consectetur adipiscing elit</a></li>
+									<li><a href="">sed do eiusmod tempor incididunt</a></li>
+									<li><a href="">sed do eiusmod tempor incididunt</a></li>
+								</ul>
+							</div>
+							<div class="no-padding">
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+							<div class="no-padding">
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+							<div class="no-padding">
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+							<div class="no-padding">
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+							<div class="dt-1-4 tb-1-2 mb-1-2">
+								<h2><a href="">Heading</a></h2>
+								<img alt="Alt text here" src="http://fpoimg.com/300x200?text=Photo" />
+								<ul>
+									<li><a href="">Lorem ipsum dolor</a></li>
+									<li><a href="">Sit sed eiusmod</a></li>
+									<li><a href="">sed do eiusmod</a></li>
+								</ul>
+							</div>
+							<div class="dt-1-4 tb-1-2 mb-1-2">
+								<h2><a href="">Heading</a></h2>
+								<img alt="Alt text here" src="http://fpoimg.com/300x200?text=Photo" />
+								<ul>
+									<li><a href="">Lorem ipsum dolor</a></li>
+									<li><a href="">Sit sed eiusmod</a></li>
+									<li><a href="">sed do eiusmod</a></li>
+								</ul>
+							</div>
+							<div class="dt-1-4 tb-1-2 mb-1-2">
+								<h2><a href="">Heading</a></h2>
+								<img alt="Alt text here" src="http://fpoimg.com/300x200?text=Photo" />
+								<ul>
+									<li><a href="">Lorem ipsum dolor</a></li>
+									<li><a href="">Sit sed eiusmod</a></li>
+									<li><a href="">sed do eiusmod</a></li>
+								</ul>
+							</div>
+							<div class="dt-1-4 tb-1-2 mb-1-2">
+								<h2><a href="">Heading</a></h2>
+								<img alt="Alt text here" src="http://fpoimg.com/300x200?text=Photo" />
+								<ul>
+									<li><a href="">Lorem ipsum dolor</a></li>
+									<li><a href="">Sit sed eiusmod</a></li>
+									<li><a href="">sed do eiusmod</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="dt-1-3 tb-1-3 mb-1-1">
+						<div class="parent dt-1-2 tb-1-2 mb-1-4 nested">
+							<div class="no-padding"><img alt="Alt text here" src="http://fpoimg.com/150x100?text=Photo" /></div>
+							<div class="no-padding"><img alt="Alt text here" src="http://fpoimg.com/150x100?text=Photo" /></div>
+							<div class="no-padding"><img alt="Alt text here" src="http://fpoimg.com/150x100?text=Photo" /></div>
+							<div class="no-padding"><img alt="Alt text here" src="http://fpoimg.com/150x100?text=Photo" /></div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+							<div class="no-padding dt-1-1 tb-1-1 mb-1-1">
+								<img alt="Alt text here" src="http://fpoimg.com/75x75?text=Photo" class="left" />
+								<h2><a href="">Headline</a></h2>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+<?php include("footer.php"); ?>
