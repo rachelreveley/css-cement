287494060d63b928d3f1cf3292f50c18c824dc61
Rachel Reveley <rachel.reveley@lovehoney.co.uk>
2015-11-26 09:17:01 +0000

    indenting

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<title><?php echo $meta_title;?></title>
 		<meta name="author" content="Rachel Reveley">
 		<link rel="stylesheet/less" type="text/css" href="styles.less" />
-		<link rel="stylesheet/less" type="text/css" href="<?php 
+		<link rel="stylesheet/less" type="text/css" href="<?php
 			if( isset($_GET["theme"]) ){ echo $_GET["theme"],"/", $_GET["theme"], ".less"; }
 			else { echo "berry/berry.less"; }
 		?>" />
@@ -28,15 +28,15 @@
 		<script src="less.js" type="text/javascript"></script>
 		<meta name="viewport" content="initial-scale=1" />
 		<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-4069218-3', 'auto');
-  ga('send', 'pageview');
+			ga('create', 'UA-4069218-3', 'auto');
+			ga('send', 'pageview');
 
-</script>
+		</script>
 	</head>
 	<body>
 		<div class="tools fixed fixed-0">
